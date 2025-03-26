e0b687415946d721983a7a7903ec3610988bc560
yu teng hsueh <jon111rock@gmail.com>
2025-03-26 15:58:38 +0800

    feat(home): 更新首頁佈局與樣式，新增暱稱設置功能

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,7 @@
 </script>
 
 <template>
-  <div class="max-w-[1280px] mx-auto p-4">
-    <header class="p-4 border-b border-gray-200 mb-4">
-      <h1 class="text-xl font-bold">DineVote</h1>
-    </header>
+  <div>
     <main>
       <router-view />
     </main>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,24 @@
 </script>
 
 <template>
-  <div class="p-2">
-    <h1 class="text-2xl font-bold mb-4">歡迎使用 DineVote</h1>
-    <p class="mb-4">這是一個幫助團體決定用餐地點的應用</p>
+  <div class="flex justify-center items-center h-screen flex-col">
+    <div class="max-w-md w-full">
+      <div class="flex flex-col items-center">
+        <h1 class="text-4xl mb-2 text-logo-gradient font-medium">DineVote</h1>
+        <p class="text-gray-600">一起決定今天吃什麼！</p>
+      </div>
+      <div class="bg-white p-8 rounded-xl shadow-lg mt-6">
+        <p class="text-xl font-medium text-gray-700">設置您的暱稱</p>
+        <div class="mt-4">
+          <p class="text-sm text-gray-600 mb-1">暱稱</p>
+          <input class="w-full border-2 border-gray-300 rounded-md p-3" type="text" placeholder="請輸入暱稱">
+        </div>
+        <button class="cursor-pointer w-full bg-red-gradient text-white rounded-md mt-4 py-3 px-4 ">儲存暱稱</button>
+      </div>
+      <div class="w-full mt-8 flex gap-4">
+        <button class="w-1/2 bg-white  rounded-md p-2 py-3 shadow-md text-gray-700 font-medium">創建房間</button>
+        <button class="w-1/2 bg-white  rounded-md p-2 py-3 shadow-md text-gray-700 font-medium">加入房間</button>
+      </div>
+    </div>
   </div>
 </template>
