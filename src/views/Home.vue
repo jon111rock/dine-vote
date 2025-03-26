49f6c971b145d57c6f12d38589dbff95f5e4a5ba
yu teng hsueh <jon111rock@gmail.com>
2025-03-26 14:52:40 +0800

    refactor: 移除不必要的元件與資源，新增路由設定與主頁面佈局

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+// 主頁元件邏輯
+</script>
+
+<template>
+  <div class="p-2">
+    <h1 class="text-2xl font-bold mb-4">歡迎使用 DineVote</h1>
+    <p class="mb-4">這是一個幫助團體決定用餐地點的應用</p>
+  </div>
+</template>
