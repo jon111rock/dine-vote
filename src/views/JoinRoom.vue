dfbdd6b06f77f692c04cb11da5bee92a30b4736f
yu teng hsueh <jon111rock@gmail.com>
2025-03-31 16:17:40 +0800

    feat(join-room): 新增加入房間頁面及導航功能
    - 在路由中新增 JoinRoom 頁面
    - 更新 Home.vue，新增導航至加入房間頁面的功能
    - 創建 JoinRoom.vue，實現房間代碼輸入介面

---
+++ src/views/JoinRoom.vue
@@ -0,0 +1,22 @@
+<script setup>
+import NavigationBack from '@/components/common/NavigationBack.vue'
+</script>
+
+<template>
+  <div class="flex items-center min-h-screen flex-col w-full py-8 px-4">
+    <div class="w-full max-w-md">
+      <NavigationBack />
+      <div class="w-full max-w-md bg-white p-6 sm:p-8 rounded-xl shadow-lg">
+        <h1 class="text-2xl font-bold text-center text-gray-800">加入房間</h1>
+        <div class="mt-6">
+          <p class="text-sm font-medium text-gray-600">房間代碼</p>
+          <input class="text-center text-lg mt-2 w-full border border-gray-300 rounded-md p-4" type="text" placeholder="請輸入房間代碼" />
+        </div>
+        <div class="mt-6">
+          <button class="cursor-pointer w-full bg-red-gradient text-white font-medium py-3 rounded-md">加入房間</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
